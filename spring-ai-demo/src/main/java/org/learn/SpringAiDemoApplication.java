fe940715b191e2e1a8917471746306b80abfcc93
naidu <chimmiri.naidu@gmail.com>
2025-11-18 18:50:51 -0600

    Initial commit

---
+++ spring-ai-demo/src/main/java/org/learn/SpringAiDemoApplication.java
@@ -0,0 +1,14 @@
+package org.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiDemoApplication.class, args);
+	}
+
+}
+

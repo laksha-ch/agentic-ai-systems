f88da50bbf8d60f94ac7d2bc0933e12089c45dc8
laksha <laksha.chimmiri@gmail.com>
2025-11-19 21:54:38 -0600

    Updated to use multi model

--- spring-ai-poc/src/main/java/org/learn/config/ChatClientConfig.java
+++ spring-ai-poc/src/main/java/org/learn/config/ChatClientConfig.java
@@ -1,22 +1,24 @@
 package org.learn.config;
 
+import org.springframework.ai.anthropic.AnthropicChatModel;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.ollama.OllamaChatModel;
+import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ChatClientConfig {
 
-//    @Bean
-//    public ChatClient openAiChatClient(OpenAiChatModel chatModel){
-//        return ChatClient.create(chatModel);
-//    }
-//
-//    @Bean
-//    public ChatClient anthropicChatClient(AnthropicChatModel chatModel){
-//        return ChatClient.create(chatModel);
-//    }
+    @Bean
+    public ChatClient openAiChatClient(OpenAiChatModel chatModel){
+        return ChatClient.create(chatModel);
+    }
+
+    @Bean
+    public ChatClient anthropicChatClient(AnthropicChatModel chatModel){
+        return ChatClient.create(chatModel);
+    }
 
     @Bean
     public ChatClient ollamaChatClient(OllamaChatModel chatModel){
